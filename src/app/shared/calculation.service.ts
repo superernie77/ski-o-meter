98350b997ec927be1c8978ddf948f30b5059605f
superernie77 <superernie77@gmail.com>
2021-03-17 10:43:56 +0100

    point calculation

--- src/app/shared/calculation.service.ts
+++ src/app/shared/calculation.service.ts
@@ -3,8 +3,6 @@ import { Calculation } from "./calculation.model";
 
 export class CalculationService {
 
-  calcCompleted : boolean = false;
-
   currentCalculation : Calculation = {points: 0, schnee: 0, wetter :"", temperatur: 0,schnee_tal:0,schnee_berg:0,tourenart :""};
 
   public setCurrentCalculation(calc : Calculation) {
@@ -25,13 +23,43 @@ export class CalculationService {
         if (schnee > 20) {
           points += 1;
         }
+
+        // Punkte für Wetter
+        var wetter = this.currentCalculation.wetter;
+        if (wetter == "Schönster Sonnenschein") {
+          points += 1;
+        } else if (wetter == "Es soll regnen"){
+          points -= 1;
+        } else if (wetter ==  "Es soll schneien"){
+          points += 1;
+        }
+
+        // Punkte für Temperatur
+        var temp = this.currentCalculation.temperatur;
+        if (temp < 0) {
+          points += 1;
+        }
+
+        // Punkte Schneemenge Tal
+        var schneeTal = this.currentCalculation.schnee_tal;
+        if (schneeTal >= 20 ) {
+          points += 1;
+        }
+
+        // Punkte Schneemenge Berg
+        var schneeBerg = this.currentCalculation.schnee_berg;
+        if (schneeTal >= 50 ) {
+          points += 1;
+        }
+
+        // Punkte Tourenart
+        var type = this.currentCalculation.tourenart;
+        if (type == "Pistentour"){
+          points += 1;
+        }
     }
-    this.calcCompleted = true;
     this.currentCalculation.points = points;
-    this.calculationReadey.emit(this.calcCompleted);
+    this.calculationReadey.emit(true);
     return points;
-
   }
-
-
 }
